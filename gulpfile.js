471a9955f02dbf2c45cb4907d041d760e651e205
John R. D'Orazio <priest@johnromanodorazio.com>
2022-01-14 15:04:49 +0100

    Create gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+/** Automate scanning for new or changed i18next.js translation keys */
+var gulp = require('gulp');
+var scanner = require('i18next-scanner');
+
+gulp.task('i18next', function() {
+    return gulp.src(['javascript/*.{js,html}'])
+        .pipe(scanner({
+            lngs: ['en', 'es', 'de', 'fr', 'it', 'pt', 'la'], // supported languages
+            resource: {
+                // the source path is relative to current working directory
+                loadPath: 'javascript/locales/{{lng}}/{{ns}}.json',
+
+                // the destination path is relative to your `gulp.dest()` path
+                savePath: 'locales/{{lng}}/{{ns}}.json'
+            }
+        }))
+        .pipe(gulp.dest('javascript'));
+});
