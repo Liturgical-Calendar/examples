687ad466f4c6f217683bdbb5874fe6ff6cd443b7
John R. D'Orazio <priest@johnromanodorazio.com>
2024-10-26 02:27:02 +0200

    debug current environment locale

--- php/index.php
+++ php/index.php
@@ -37,9 +37,12 @@ if (false === $directAccess) {
     }
 }
 
+// debug value of current environment locale
+echo '<!-- current environment locale: ' . setlocale(LC_ALL, 0) . ' -->';
+
 $litSettings = new LitSettings($_GET, $directAccess);
 
-// debug value of locale that the script was set to
+// debug value of locale that the script was set to after initializing LitSettings
 echo '<!-- setLocale: ' . $litSettings->setLocale . ' -->';
 
 $nationalCalendarOptions = '<option value="">---</option>';
